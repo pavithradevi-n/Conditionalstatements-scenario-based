6eb444fead5da1a76f3d58d449de1f069e6d43a1
pavithradevi-n <sumathipavithra1234@gmail.com>
2025-12-15 20:13:37 +0530

    add

--- program14.java
+++ program14.java
@@ -1,1 +1,10 @@
-\
+import java.util.Scanner;
+class TicketPrice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if (age < 12) System.out.println("Ticket: ₹50");
+        else if (age < 60) System.out.println("Ticket: ₹100");
+        else System.out.println("Ticket: ₹70");
+    }
+}
